7992d473eec455bbff35c17ea413aaca7cf1b6ac
Kevin <yinkai1205@163.com>
2014-08-25 16:15:53 +0800

    NiftyDialog
    自定义Dialog, 支持各种动画方式弹出

---
+++ gen/com/gitonway/lee/niftymodaldialogeffects/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.gitonway.lee.niftymodaldialogeffects;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
